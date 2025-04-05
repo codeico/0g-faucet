ff206bb771fae6d68884b229ef33890f4863bdf8
codeico <bangico2002@gmail.com>
2025-04-05 07:10:57 +0700

    Update Error 3

--- components/Faucet.tsx
+++ components/Faucet.tsx
@@ -47,9 +47,9 @@ export default function Faucet() {
               </p>
             </div>
             <div className="bg-[#232836] p-4 mx-6 mb-6 rounded-lg border-l-4 border-[#FFB800]">
-              <div class="flex"><div class="flex-shrink-0"><svg class="h-5 w-5 text-[#FFB800]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg></div>
-                <div class="ml-3"><h3 class="text-sm font-medium text-[#FFB800]">
-                  Faucet Limit</h3><div class="mt-1 text-xs text-gray-400">
+              <div className="flex"><div className="flex-shrink-0"><svg className="h-5 w-5 text-[#FFB800]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg></div>
+                <div className="ml-3"><h3 className="text-sm font-medium text-[#FFB800]">
+                  Faucet Limit</h3><div className="mt-1 text-xs text-gray-400">
                     You can request 1 0G token every 24 hours per address.</div>
                 </div>
               </div>
@@ -64,7 +64,7 @@ export default function Faucet() {
                 <HCaptcha sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY as string} onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)} />
               </div>
               <div>
-                <button disabled={isDisabled} type="submit" classNamex="group relative flex w-full justify-center rounded-lg border border-transparent bg-[#5170FF] py-3 px-4 text-sm font-semibold text-white hover:bg-[#4060EE] focus:outline-none focus:ring-2 focus:ring-[#5170FF]/50 transition-all duration-200 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed mb-2"><span class="absolute inset-y-0 left-0 flex items-center pl-3"><svg class="h-5 w-5 text-[#B4C6FF] group-hover:text-[#D1DCFF]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg></span>
+                <button disabled={isDisabled} type="submit" className="group relative flex w-full justify-center rounded-lg border border-transparent bg-[#5170FF] py-3 px-4 text-sm font-semibold text-white hover:bg-[#4060EE] focus:outline-none focus:ring-2 focus:ring-[#5170FF]/50 transition-all duration-200 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed mb-2"><span class="absolute inset-y-0 left-0 flex items-center pl-3"><svg class="h-5 w-5 text-[#B4C6FF] group-hover:text-[#D1DCFF]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg></span>
                   Request Tokens
                 </button>
               </div>
