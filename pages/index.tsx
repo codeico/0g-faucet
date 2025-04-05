6d40b57a8073fedc6e9ff32231e5f7ae7af02201
codeico <bangico2002@gmail.com>
2025-04-05 07:28:13 +0700

    Update Tampilan 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Head from "next/head";
+import Faucet from "../components/Faucet";
 
-export default function Document() {
+export default function Home() {
   return (
-    <Html lang="en">
+    <>
       <Head>
+        <title>BANGCODE | 0G Testnet Faucet</title>
+        <meta name="description" content="BANGCODE | 0G Testnet Faucet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/0g.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body className="bg-[#151923] text-white font-sans">
-        <Main />
-        <NextScript />
+        <main>
+          <Faucet />
+        </main>
       </body>
-    </Html>
+    </>
   );
 }
