3cafac6de2e4cc15c2b5b31bc7539be793bfb7eb
BANGCODE <159583194+codeico@users.noreply.github.com>
2025-04-19 20:47:53 +0700

    Update next.config.js allow indexing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // berlaku untuk semua path
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow', // Mengizinkan pengindeksan dan mengikuti link
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
